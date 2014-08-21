d7197cdbc1122e348f7e8c130f13372523f90be9
Christopher Giroir <cgiroir@blizzard.com>
2014-08-21 08:39:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Passport Bnet
+
+This is an Passport strategy for authenticating to Blizzard's Battle.net OAuth
+service. In order to use it you need to register an application at the
+[Battle.net Developer Portal](https://dev.battle.net)
+
+## Installation
+
+    $ npm install passport-bnet
+
+## Usage
+
+### Configure
+
+    var BnetStrategy = require('passport-bnet').Strategy;
+    var BNET_ID = process.env.BNET_ID
+    var BNET_SECRET = process.env.BNET_SECRET
+
+    // Use the BnetStrategy within Passport.
+    passport.use(new BnetStrategy({
+                         clientID: BNET_ID,
+                         clientSecret: BNET_SECRET,
+                         callbackURL: "https://localhost:3000/auth/bnet/callback"
+                     }, function(accessToken, refreshToken, profile, done) {
+                         return done(null, profile);
+                     }));
+
+### Authenticate Requests
+
+    app.get('/auth/bnet',
+        passport.authenticate('bnet'));
+
+    app.get('/auth/bnet/callback',
+        passport.authenticate('bnet', { failureRedirect: '/' }),
+        function(req, res){
+            res.redirect('/');
+        });
+
+## License
+
+[The MIT License](http://opensource.org/licenses/MIT)
---
+++ examples/express/app.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var passport = require('passport');
+var util = require('util');
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var BnetStrategy = require('passport-bnet').Strategy;
+var GitHubStrategy = require('passport-github').Strategy;
+
+var GITHUB_ID = process.env.GITHUB_ID;
+var GITHUB_SECRET = process.env.GITHUB_SECRET;
+var BNET_ID = process.env.BNET_ID;
+var BNET_SECRET = process.env.BNET_SECRET;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+// Use the GitHubStrategy within Passport.
+passport.use(
+  new GitHubStrategy(
+    { clientID: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
+      callbackURL: "https://local.test.battle.net/auth/github/callback" },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    })
+);
+
+// Use the BnetStrategy within Passport.
+passport.use(
+  new BnetStrategy(
+    { clientID: BNET_ID,
+      clientSecret: BNET_SECRET,
+      scope: "wow.profile sc2.profile",
+      callbackURL: "https://local.test.battle.net/auth/bnet/callback" },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    })
+);
+
+var app = express();
+
+// configure Express
+app.use(cookieParser());
+app.use(session({ secret: 'blizzard',
+                  saveUninitialized: true,
+                  resave: true }));
+
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+        passport.authenticate('github', { failureRedirect: '/' }),
+        function(req, res){
+          res.redirect('/');
+        });
+
+app.get('/auth/bnet',
+        passport.authenticate('bnet'));
+
+app.get('/auth/bnet/callback',
+        passport.authenticate('bnet', { failureRedirect: '/' }),
+        function(req, res){
+          res.redirect('/');
+        });
+
+app.get('/', function(req, res) {
+  if(req.isAuthenticated()) {
+    var output = '<h1>Express OAuth Test</h1>' + req.user.id + '<br>';
+    if(req.user.battletag) {
+      output += req.user.battletag + '<br>';
+    }
+    output += '<a href="/logout">Logout</a>';
+    res.send(output);
+  } else {
+    res.send('<h1>Express OAuth Test</h1>' +
+             '<a href="/auth/github">Login with Github</a><br>' +
+             '<a href="/auth/bnet">Login with Bnet</a>');
+  }
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
