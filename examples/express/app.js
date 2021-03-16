283180d151f23ab71bed43f8b62eba85457e1388
Lior Koren <45363723+HirboSH@users.noreply.github.com>
2021-03-16 18:31:52 +0200

    :star: Updated express example :star:
    Did a deep explanation of that code, and simplified it a bit.

--- examples/express/app.js
+++ examples/express/app.js
@@ -1,27 +1,49 @@
-var express = require('express');
-var passport = require('passport');
-var util = require('util');
+/* ====
+    [Packages] - Packages required.
+==== */
 
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+const express = require('express');
+const passport = require('passport');
+const util = require('util');
 
-var BnetStrategy = require('passport-bnet').Strategy;
-var GitHubStrategy = require('passport-github').Strategy;
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var GITHUB_ID = process.env.GITHUB_ID;
-var GITHUB_SECRET = process.env.GITHUB_SECRET;
-var BNET_ID = process.env.BNET_ID;
-var BNET_SECRET = process.env.BNET_SECRET;
+/* ====
+    [Packages] - Require our passport packages.
+==== */
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
+const BnetStrategy = require('passport-bnet').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
 
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
+/* ====
+    [Vars] - SECRET(s) / ID(s) for our strategies. 
+      GITUHB_? -> for our Github strategy.
+      BNET_? -> for our BNET strategy.
+==== */
 
-// Use the GitHubStrategy within Passport.
+const GITHUB_ID = process.env.GITHUB_ID;
+const GITHUB_SECRET = process.env.GITHUB_SECRET;
+const BNET_ID = process.env.BNET_ID;
+const BNET_SECRET = process.env.BNET_SECRET;
+
+/* ====
+    [Express] - Setup our express server.
+==== */
+
+const app = express();
+
+/* ====
+    [Passport] - Serialize / Deserialize our user when requests are made.
+==== */
+
+passport.serializeUser( function(user, done) { done(null, user);  });
+passport.deserializeUser( function(obj, done) { done(null, obj); } );
+
+/* ====
+    [Github-Strategy] - Use the `GitHubStrategy` within the passport.
+==== */
+
 passport.use(
   new GitHubStrategy(
     { clientID: GITHUB_ID,
@@ -34,7 +56,10 @@ passport.use(
     })
 );
 
-// Use the BnetStrategy within Passport.
+/* ====
+    [BNET-Strategy] - Use the `BnetStrategy` within the passport.
+==== */
+
 passport.use(
   new BnetStrategy(
     { clientID: BNET_ID,
@@ -48,56 +73,64 @@ passport.use(
     })
 );
 
-var app = express();
+/* ====
+    [Express] - Init and configure express.
+    [Passport] - Init passport. Use -> passport.session() middleware, to support persistent login sessions (recommended).
+==== */
 
-// configure Express
 app.use(cookieParser());
-app.use(session({ secret: 'blizzard',
-                  saveUninitialized: true,
-                  resave: true }));
+app.use(session({ secret: 'blizzard', saveUninitialized: true, resave: true }));
 
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
 
+/* ====
+    [Express] - Setup our routes for our strategies.
+==== */
+
 app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), function(req, res){ res.redirect('/'); });
 
-app.get('/auth/github/callback',
-        passport.authenticate('github', { failureRedirect: '/' }),
-        function(req, res){
-          res.redirect('/');
-        });
+app.get('/auth/bnet', passport.authenticate('bnet'));
+app.get('/auth/bnet/callback', passport.authenticate('bnet', { failureRedirect: '/' }), function(req, res){ res.redirect('/'); });
 
-app.get('/auth/bnet',
-        passport.authenticate('bnet'));
-
-app.get('/auth/bnet/callback',
-        passport.authenticate('bnet', { failureRedirect: '/' }),
-        function(req, res){
-          res.redirect('/');
-        });
+/* ====
+    [Express] - Setup our 'logged in successfully' / 'need to log in' route.
+      req.isAuthenticated() -> Indicator.
+==== */
 
 app.get('/', function(req, res) {
-  if(req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     var output = '<h1>Express OAuth Test</h1>' + req.user.id + '<br>';
-    if(req.user.battletag) {
-      output += req.user.battletag + '<br>';
-    }
+      if (req.user.battletag) {
+        output += req.user.battletag + '<br>';
+      }
     output += '<a href="/logout">Logout</a>';
     res.send(output);
   } else {
-    res.send('<h1>Express OAuth Test</h1>' +
-             '<a href="/auth/github">Login with Github</a><br>' +
-             '<a href="/auth/bnet">Login with Bnet</a>');
+    res.send('<h1>Express OAuth Test</h1>' + '<a href="/auth/github">Login with Github</a><br>' + '<a href="/auth/bnet">Login with Bnet</a>');
   }
 });
 
+/* ====
+    [Express] - Setup our 'logout' route.
+      req.isAuthenticated() -> Indicator if our user logged in or not.
+==== */
+
 app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+  if (req.isAuthenticated()) {
+    req.logout();
+    res.redirect('/');
+  } else {
+    res.redirect('/');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
-});
+/* ====
+    [Express] - 'Start' our server.
+==== */
+
+const server = app.listen(3000, function() { console.log('[Blizzard-OAuth] Listening on port %d', server.address().port); });
+
+/* ====
+    [DONE] - DONE.
+==== */
